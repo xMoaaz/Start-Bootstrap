b961925fff8d496b7e1fff15070ba4aaf32d5061
xMoaaz <moaazblqacey@std.mans.edu.eg>
2023-10-18 20:02:53 +0300

    create project

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("copy-btn");
+
+btn.onclick = function show() {
+    document.querySelector(".copy-btnx").style.display = "block";
+    document.querySelector(".xcopy-btn").style.display = "block";
+}
